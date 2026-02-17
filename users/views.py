3bb976d4578223a367460c5a564d50ac45f6a2d8
rineluo920-svg <rineluo920@gmail.com>
2026-02-17 14:12:37 +1100

    Http Requests

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+@login_required(login_url='users:login')
+def user(request):
+    return render(request, "tasks/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.GET.get('next', reverse("users:user"))
+            return HttpResponseRedirect(next_url)
+        else:
+            messages.error(request, "Invalid Credentials.")
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Successfully logged out.")
+    return redirect('users:login')
